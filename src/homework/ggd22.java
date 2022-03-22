de1b947371472203d9217d5fc00623d1dcae905b
TKMTKJ <101791458+TKMTKJ@users.noreply.github.com>
2022-03-22 15:19:33 +0900

    Add files via upload

---
+++ src/homework/ggd22.java
@@ -0,0 +1,16 @@
+package d0322;
+
+public class ggd22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("구구단");
+		for (int j=1; j<=9;j++){
+			for(int i=2; i<=9; i++) 
+			{
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+			}
+			System.out.println("");
+	}
+	}
+}
